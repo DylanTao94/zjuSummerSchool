3ff369f27985455de365606d79b26c0cd50da16a
DylanTao94 <dylan.tao94@gmail.com>
2020-09-11 13:57:37 +1000

    成功连接图灵服务器

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,27 +1,34 @@
 const express = require('express'), //express 框架 
-      wechat  = require('./wechat/wechat'), 
-       config = require('./config');//引入配置文件
-       
+    wechat = require('./wechat/wechat'),
+    config = require('./config'),//引入配置文件
+    xmlparser = require('express-xml-bodyparser');//引入xml解析中间件
+
 var app = express();//实例express框架
+// 为处理XML信息，加入中间件，预处理
+app.use(express.json());
+// app.use(express.urlencoded());
+app.use(xmlparser());
 
 var wechatApp = new wechat(config); //实例wechat 模块
 
 //用于处理所有进入 3000 端口 get 的连接请求
-app.get('/',function(req,res){
-    wechatApp.auth(req,res);
+app.get('/', function (req, res) {
+    wechatApp.auth(req, res);
 });
 
 //用于处理所有进入 3000 端口 post 的连接请求
-app.post('/',function(req,res){
-    wechatApp.handleMsg(req,res);
+app.post('/', function (req, res) {
+    wechatApp.handleMsg(req, res);
 });
 
 //用于请求获取 access_token
-app.get('/getAccessToken',function(req,res){
-    wechatApp.getAccessToken().then(function(data){
+app.get('/getAccessToken', function (req, res) {
+    wechatApp.getAccessToken().then(function (data) {
         res.send(data);
-    });    
+    });
 });
 
 //监听3000端口
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Listening port : 3000")
+});
--- backEnd/wechat/wechat.js
+++ backEnd/wechat/wechat.js
@@ -30,6 +30,13 @@ var WeChat = function (config) {
     //设置 WeChat 对象属性 apiURL
     this.apiURL = config.apiURL;
 
+
+
+    //设置 图灵机器人接口 对象属性 turningAIURL
+    this.turningAIURL = config.turningAIURL;
+    //设置 图灵机器人接口 对象属性 turningAIURL
+    this.turningAIApiKey = config.turningAIApiKey;
+
     /**
      * 用于处理 https Get请求方法
      * @param {String} url 请求地址 
@@ -181,92 +188,113 @@ WeChat.prototype.getAccessToken = function () {
  * @param {Response} res Response 对象
  */
 WeChat.prototype.handleMsg = function (req, res) {
-    // 此处获取用户openid
-    console.log(req.query.openid);
-    var buffer = [], that = this;
+    /*********************
+     * xml解析后的数据结构如下
+     * {
+     *    tousername: [ 'gh_ea8ddcbb7bda' ],
+     *    fromusername: [ 'ostUYt3Yp7Paq2QM7nFNoH6GAi70' ],
+     *    createtime: [ '1599787083' ],
+     *    msgtype: [ 'text' ],
+     *    content: [ 's' ],
+     *    msgid: [ '22903442057179070' ]
+     * }
+     *********************/
+    var result = {};
 
-    //实例微信消息加解密
-    var cryptoGraphy = new CryptoGraphy(that.config, req);
+    result.toUserName = req.body.xml.tousername[0]; //接收方微信
+    result.fromUserName = req.body.xml.fromusername[0];//发送方openId
+    result.createTime = req.body.xml.createtime[0]//发送时间
+    result.msgType = req.body.xml.msgtype[0]//发送消息类型
+    // 不同消息可能不含有以下信息，需先进行判断
+    if (req.body.xml.content) {
+        result.content = req.body.xml.content[0]//发送消息内容
+    }
+    if (req.body.xml.msgId) {
+        result.msgId = req.body.xml.msgid[0]//发送消息
+    }
+    if (req.body.xml.event) {
+        result.event = req.body.xml.event[0]//发送消息
+    }
+    var reportMsg = ""; //声明回复消息的变量 s  
+    var data2truningAI = {}; //声明传递给图灵机器人的数据
+    //判断消息类型
+    switch (result.msgType) {
+        case "event": //判断事件类型为event
+            switch (result.event) {
+                case 'subscribe':
+                    //回复消息
+                    var content = "欢迎关注 浙大暑校 公众号，可以直接给客服发送信息，客服会立马回复你想要的数据哟。如回复：\n";
+                    content += "暑期课介绍\n";
+                    content += "暑期课上课地点\n";
+                    content += "课程介绍\n";
+                    reportMsg = msg.txtMsg(result.fromUserName, result.toUserName, content);
+                    break;
+                case 'click':
+                    var contentArr = [
+                        { Title: "Node.js 微信自定义菜单", Description: "使用Node.js实现自定义微信菜单", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72868520" },
+                        { Title: "Node.js access_token的获取、存储及更新", Description: "Node.js access_token的获取、存储及更新", PicUrl: "http://img.blog.csdn.net/20170528151333883?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72783631" },
+                        { Title: "Node.js 接入微信公众平台开发", Description: "Node.js 接入微信公众平台开发", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72765279" }
+                    ];
+                    //回复图文消息
+                    reportMsg = msg.graphicMsg(result.fromUserName, result.toUserName, contentArr);
 
-    //监听 data 事件 用于接收数据
-    req.on('data', function (data) {
-        buffer.push(data);
-    });
-    //监听 end 事件 用于处理接收完成的数据
-    req.on('end', function () {
-        var msgXml = Buffer.concat(buffer).toString('utf-8');
-        //解析xml
-        parseString(msgXml, { explicitArray: false }, function (err, result) {
-            if (!err) {
-                result = result.xml;
-                //判断消息加解密方式
-                if (req.query.encrypt_type == 'aes') {
-                    //对加密数据解密
-                    result = cryptoGraphy.decryptMsg(result.Encrypt);
-                }
-                var toUser = result.ToUserName; //接收方微信
-                var fromUser = result.FromUserName;//发送仿微信
-                var reportMsg = ""; //声明回复消息的变量   
-
-                //判断消息类型
-                if (result.MsgType.toLowerCase() === "event") {
-                    //判断事件类型
-                    switch (result.Event.toLowerCase()) {
-                        case 'subscribe':
-                            //回复消息
-                            var content = "欢迎关注 hvkcoder 公众号，一起斗图吧。回复以下数字：\n";
-                            content += "1.你是谁\n";
-                            content += "2.关于Node.js\n";
-                            content += "回复 “文章”  可以得到图文推送哦~\n";
-                            reportMsg = msg.txtMsg(fromUser, toUser, content);
-                            break;
-                        case 'click':
-                            var contentArr = [
-                                { Title: "Node.js 微信自定义菜单", Description: "使用Node.js实现自定义微信菜单", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72868520" },
-                                { Title: "Node.js access_token的获取、存储及更新", Description: "Node.js access_token的获取、存储及更新", PicUrl: "http://img.blog.csdn.net/20170528151333883?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72783631" },
-                                { Title: "Node.js 接入微信公众平台开发", Description: "Node.js 接入微信公众平台开发", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72765279" }
-                            ];
-                            //回复图文消息
-                            reportMsg = msg.graphicMsg(fromUser, toUser, contentArr);
-                            break;
+                    break;
+            }
+            break;
+            //返回给微信服务器
+            res.send(reportMsg);
+        case "text": //判断消息类型为 文本消息
+            // 根据消息内容返回消息信息
+            /**
+             * 处理Json数据发送给turingAI
+             * 请求方式：POST
+             * URL：http://openapi.tuling123.com/openapi/api/v2
+             * 请求参数：参考图灵接口文档 https://www.kancloud.cn/turing/www-tuling123-com/718227
+             **/
+            // 处理Json数据发送给turingAI
+            data2truningAI = {
+                "reqType": 0, // 输入类型:0-文本(默认)、1-图片、2-音频Î
+                perception: {
+                    "inputText": {
+                        "text": result.content,
                     }
-                } else {
-                    //判断消息类型为 文本消息
-                    if (result.MsgType.toLowerCase() === "text") {
-                        //根据消息内容返回消息信息
-                        switch (result.Content) {
-                            case '1':
-                                reportMsg = msg.txtMsg(fromUser, toUser, 'Hello ！我的英文名字叫 H-VK');
-                                break;
-                            case '2':
-                                reportMsg = msg.txtMsg(fromUser, toUser, 'Node.js是一个开放源代码、跨平台的JavaScript语言运行环境，采用Google开发的V8运行代码,使用事件驱动、非阻塞和异步输入输出模型等技术来提高性能，可优化应用程序的传输量和规模。这些技术通常用于数据密集的事实应用程序');
-                                break;
-                            case '文章':
-                                var contentArr = [
-                                    { Title: "Node.js 微信自定义菜单", Description: "使用Node.js实现自定义微信菜单", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72868520" },
-                                    { Title: "Node.js access_token的获取、存储及更新", Description: "Node.js access_token的获取、存储及更新", PicUrl: "http://img.blog.csdn.net/20170528151333883?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72783631" },
-                                    { Title: "Node.js 接入微信公众平台开发", Description: "Node.js 接入微信公众平台开发", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72765279" }
-                                ];
-                                //回复图文消息
-                                reportMsg = msg.graphicMsg(fromUser, toUser, contentArr);
-                                break;
-                            default:
-                                reportMsg = msg.txtMsg(fromUser, toUser, '没有这个选项哦');
-                                break;
-                        }
-                    }
+                },
+                "userInfo": {
+                    "apiKey": this.turningAIApiKey,
+                    "userId": result.fromUserName
                 }
-                //判断消息加解密方式，如果未加密则使用明文，对明文消息进行加密
-                reportMsg = req.query.encrypt_type == 'aes' ? cryptoGraphy.encryptMsg(reportMsg) : reportMsg;
+            };
+            // 发送json数据到图灵AI服务器
+            this.requestPost(this.turningAIURL, JSON.stringify(data2truningAI)).then(function (data) {
+                // 图灵机器人返回的数据格式为：
+                // var data = {
+                //     "intent":
+                //     {
+                //         "actionName": "",
+                //         "code": 10020,
+                //         "intentName": ""
+                //     },
+                //     "results": [
+                //         {
+                //             "groupType": 1,
+                //             "resultType": "text",
+                //             "values":
+                //             {
+                //                 "text": "作为"
+                //             }
+                //         }
+                //     ]
+                // }
+                // 将数据解析成JSON
+                let data_JSON = JSON.parse(data);
+                // 将数据处理成为发送给微信的格式
+                reportMsg = msg.txtMsg(result.fromUserName, result.toUserName, data_JSON.results[0].values.text);
                 //返回给微信服务器
                 res.send(reportMsg);
+            });
+    }
 
-            } else {
-                //打印错误
-                console.log(err);
-            }
-        });
-    });
+    // res.send(reportMsg);
 }
 
 //暴露可供外部访问的接口
